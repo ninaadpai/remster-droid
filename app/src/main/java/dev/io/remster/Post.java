3bffbd6d3d2a019df4a8edb302008db6adc101f8
Ninaad Pai <paininaad@gmail.com>
2017-05-04 01:25:51 -0700

    Too many additions to recall.

---
+++ app/src/main/java/dev/io/remster/Post.java
@@ -0,0 +1,8 @@
+package dev.io.remster;
+
+/**
+ * Created by ninaadpai on 5/3/17.
+ */
+
+public class Post {
+}
---
+++ app/src/main/java/dev/io/remster/Preview.java
@@ -0,0 +1,8 @@
+package dev.io.remster;
+
+/**
+ * Created by ninaadpai on 5/1/17.
+ */
+
+public class Preview {
+}
